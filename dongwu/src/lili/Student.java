cc83c347518ae883c1d1ac93a518e7b306c09767
tianshizhe <1426103177@qq.com>
2016-12-11 16:37:09 +0800

    Signed-off-by: tianshizhe<tianshizhe@qq.com>

---
+++ dongwu/src/lili/Student.java
@@ -0,0 +1,21 @@
+package lili;
+
+public class Student extends texing {
+   int score;
+  void shuchus()
+  {
+	  System.out.println("Ìï");
+  }
+  static void asd(){
+	  System.out.print("hello");
+  }
+  public void s(String a) {
+	  System.out.print(a);
+	}
+public int getScore() {
+	return score;
+}
+public void setScore(int score) {
+	this.score = score;
+}
+}
